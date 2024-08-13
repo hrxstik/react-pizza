c17526ffea592e20043961d350455a0aabfefb3a
hrxstik <hisamov2004@gmail.com>
2024-08-13 22:00:16 +0500

    props+useState

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
+import pizzas from './assets/pizzas.json';
+
 function App() {
   return (
     <div className="App">
-      <div class="wrapper">
+      <div className="wrapper">
         <Header />
-        <div class="content">
-          <div class="container">
-            <div class="content__top">
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
               <Categories />
               <Sort />
             </div>
-            <h2 class="content__title">Все пиццы</h2>
-            <div class="content__items">
-              <PizzaBlock title="Мексиканская" price={500} />
-              <PizzaBlock title="Чизбургер-пицца" price={400} />
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {pizzas.map((pizza) => (
+                <PizzaBlock {...pizza} />
+              ))}
             </div>
           </div>
         </div>
